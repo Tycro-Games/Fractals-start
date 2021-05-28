192a000d0840caa0e448582dd120b94036f1a020
NiceGuy <bogdan.game.development@gmail.com>
2021-05-28 10:22:56 +0300

    Advanced and manual parenting

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -8,38 +8,112 @@ public class Fractal : MonoBehaviour
     [SerializeField]
     private float offset = 1.0f;
 
-    private void Start()
-    {
-        name = "Celula " + depth;
-        if (depth <= 1)
-        {
-            return;
-        }
-        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
-        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
-        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
-        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0f));
-        Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0f));
+    [SerializeField]
+    private Mesh[] mesh;
 
-        childA.transform.SetParent(transform, false);
-        childB.transform.SetParent(transform, false);
-        childC.transform.SetParent(transform, false);
-        childD.transform.SetParent(transform, false);
-        childE.transform.SetParent(transform, false);
+    [SerializeField]
+    private Material material;
+
+    private MaterialPropertyBlock _propBlock;
+
+    [ColorUsage(true, true)]
+    public Color Color1, Color2;
+
+    public float Speed = 1, Offset;
+
+    private FractalPart[][] parts;
+    public float rotspeed;
+
+    private struct FractalPart
+    {
+        public Vector3 direction;
+        public Quaternion rotation;
+        public Transform transform;
+    }
+
+    private static Vector3[] directions = {
+        Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back
+    };
+
+    private static Quaternion[] rotations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f)
+    };
+
+    private void Awake()
+    {
+        parts = new FractalPart[depth][];
+
+        for (int i = 0, length = 1; i < parts.Length; i++, length *= 5)
+        {
+            parts[i] = new FractalPart[length];//init all the sub arrays
+        }
+        float scale = 1f;//base scale
+        parts[0][0] = CreatePart(0, 0, scale);//first one
+        for (int l1 = 1; l1 < parts.Length; l1++)//generates in all 5 directions
+        {
+            scale *= 0.5f;
+            FractalPart[] levelParts = parts[l1];
+            for (int j = 0; j < levelParts.Length; j += 5)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    levelParts[j + c] = CreatePart(l1, c, scale);
+                }
+            }
+        }
     }
 
     private void Update()
     {
-        //   transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
+        Quaternion deltaRotation = Quaternion.Euler(0f, rotspeed * Time.deltaTime, 0f);
+        FractalPart rootPart = parts[0][0];
+        rootPart.rotation *= deltaRotation;
+        rootPart.transform.localRotation = rootPart.rotation;
+        parts[0][0] = rootPart;
+
+        for (int l1 = 1; l1 < parts.Length; l1++)
+        {
+            FractalPart[] parentParts = parts[l1 - 1];
+            FractalPart[] levelParts = parts[l1];
+            for (int j = 0; j < levelParts.Length; j++)
+            {
+                Transform parentTransform = parentParts[j / 5].transform;
+                FractalPart part = levelParts[j];
+                part.rotation *= deltaRotation;
+                //pos rot
+                part.transform.localRotation = parentTransform.localRotation * part.rotation;//so it is local
+                part.transform.localPosition =
+                    parentTransform.localPosition +
+                    parentTransform.localRotation *
+                       (offset * part.transform.localScale.x * part.direction);
+                levelParts[j] = part;
+            }
+        }
     }
 
-    private Fractal CreateChild(Vector3 direction, Quaternion rotation)//constructor
+    private FractalPart CreatePart(int levelIndex, int childIndex, float scale)
     {
-        Fractal child = Instantiate(this); //make this
-        child.depth = depth - 1; //substract so it's not infinite
-        child.transform.localPosition = direction * offset;
-        child.transform.localRotation = rotation;
-        child.transform.localScale = 0.5f * Vector3.one; //yep it is constant(cause in hierarchy a parent dictates the world scale
-        return child;
+        var go = new GameObject("Celula" + levelIndex + " C" + childIndex);
+        go.transform.localScale = scale * Vector3.one;
+        go.transform.SetParent(transform, false);
+
+        go.AddComponent<MeshFilter>().mesh = mesh[Random.Range(0, mesh.Length)];
+
+        MeshRenderer meshRenderer = go.AddComponent<MeshRenderer>();
+        meshRenderer.material = material;
+        //change color
+        _propBlock = new MaterialPropertyBlock();
+        meshRenderer.GetPropertyBlock(_propBlock);
+        Speed += .5f;
+        _propBlock.SetColor("_BaseColor", Color.Lerp(Color1, Color2, (Mathf.Sin(Speed + Offset) + 1) / 2f));
+        meshRenderer.SetPropertyBlock(_propBlock);
+        return new FractalPart
+        {
+            direction = directions[childIndex],
+            rotation = rotations[childIndex],
+            transform = go.transform
+        };
     }
 }
